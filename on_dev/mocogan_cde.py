676918015d4c97c82dbc35eaa47028d3594027b4
Chinh La <chinh.la@anu.edu.au>
2021-10-16 14:19:22 +1100

    init cde to test train

--- on_dev/mocogan_cde.py
+++ on_dev/mocogan_cde.py
@@ -32,12 +32,12 @@ class CDEFunc(nn.Module):
 
 class VideoGeneratorCDE(VideoGeneratorMNIST):
     def __init__(self,n_channels, dim_z_content, dim_z_category, dim_z_motion,
-                 video_length, ode_fn=CDEFunc, dim_hidden=None, linear=True,ngf=64):
+                 video_length, ode_fn=CDEFunc, cde_input_dim = 2, dim_hidden=None, linear=True,ngf=64):
         super().__init__(n_channels, dim_z_content, dim_z_category, dim_z_motion, video_length,ngf=ngf)
         if dim_hidden:
-            self.ode_fn = ode_fn(dim=dim_z_motion, dim_hidden=dim_hidden)
+            self.ode_fn = ode_fn(dim=cde_input_dim, dim_hidden=dim_hidden)
         else:
-            self.ode_fn = ode_fn(dim=dim_z_motion, dim_hidden = dim_z_motion)
+            self.ode_fn = ode_fn(dim=cde_input_dim, dim_hidden = dim_z_motion)
         
         if linear:
             self.linear = nn.Sequential(
@@ -58,20 +58,23 @@ class VideoGeneratorCDE(VideoGeneratorMNIST):
 
     def sample_z_m(self, num_samples, video_len=None):
         video_len = video_len if video_len is not None else self.video_length
-        # generate initial Z
-        # z_0 = self.get_gru_initial_state(num_samples)
         # generate X
         X_t = self.get_iteration_noise(num_samples)
-        batch, z_dim = X_t.size()
+        # get batch size
+        batch, _ = X_t.size()
         t = torch.linspace(0,1,video_len)
+        # generate time
         t_ = t.repeat(batch).view(batch,video_len,1)
         x_ = X_t.view(batch,video_len,1)
+        # IMPORTANCE: concatenate time with x 
         x = torch.cat([t_,x_],dim=2)
+        # use torchcde to generate
         coeffs = torchcde.hermite_cubic_coefficients_with_backward_differences(x)
         X = torchcde.CubicSpline(coeffs)
+        # z0 should be a function of x
         z0 = self.f(X.evaluate(X.interval[0]))
-        z_T = torchcde.cdeint(X=X,z0=z0,func=cde_fn,t=torch.arange(0,video_len).float())
-
+        z_T = torchcde.cdeint(X=X,z0=z0,func=self.ode_fn,t=torch.arange(0,video_len).float())
+        # reshape to match output size
         z_T = z_T.view(-1,video_len)
 
         return z_T
